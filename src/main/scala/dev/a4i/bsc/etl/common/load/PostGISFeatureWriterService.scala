25909b5ff9921573a57b45a36fe604c95453906a
Achilleas Naoumidis <achinaou@gmail.com>
2025-07-08 22:46:31 +0300

    Add transaction for better performance

--- src/main/scala/dev/a4i/bsc/etl/common/load/PostGISFeatureWriterService.scala
+++ src/main/scala/dev/a4i/bsc/etl/common/load/PostGISFeatureWriterService.scala
@@ -1,9 +1,11 @@
 package dev.a4i.bsc.etl.common.load
 
 import java.io.IOException
+import java.util.UUID
 
 import org.geotools.api.data.SimpleFeatureStore
 import org.geotools.api.feature.simple.SimpleFeatureType
+import org.geotools.data.DefaultTransaction
 import org.geotools.data.simple.SimpleFeatureCollection
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder
 import zio.*
@@ -22,8 +24,10 @@ class PostGISFeatureWriterService(dataStore: PostGISDataStore):
                                                dataStore
                                                  .getFeatureSource(tableName)
                                                  .asInstanceOf[SimpleFeatureStore]
-      _                                   <- ZIO.attemptBlockingIO:
-                                               featureStore.addFeatures(featureCollection)
+      _                                   <- usingTransaction: transaction =>
+                                               ZIO.attemptBlockingIO:
+                                                 featureStore.setTransaction(transaction)
+                                                 featureStore.addFeatures(featureCollection)
     yield ()
 
   private def getTargetFeatureType(tableName: String, sourceFeatureType: SimpleFeatureType): SimpleFeatureType =
@@ -36,6 +40,15 @@ class PostGISFeatureWriterService(dataStore: PostGISDataStore):
     ZIO.attemptBlockingIO:
       if !dataStore.getTypeNames.contains(tableName)
       then dataStore.createSchema(featureType)
+
+  private def usingTransaction[R, E, A](use: DefaultTransaction => ZIO[R, E, A]): ZIO[R, E, A] =
+    val acquire: UIO[DefaultTransaction]         = ZIO.succeed(DefaultTransaction(UUID.randomUUID.toString))
+    val release: DefaultTransaction => UIO[Unit] = transaction => ZIO.succeed(transaction.close())
+
+    ZIO.acquireReleaseWith(acquire)(release): transaction =>
+      use(transaction)
+        .zipLeft(ZIO.succeed(transaction.commit()))
+        .onError(_ => ZIO.succeed(transaction.rollback()))
 
 object PostGISFeatureWriterService:
 
