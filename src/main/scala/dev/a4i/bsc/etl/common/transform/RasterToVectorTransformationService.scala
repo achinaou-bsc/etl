b1340d424e462368052f5dd687b8189ee0cf37f4
Achilleas Naoumidis <achinaou@gmail.com>
2025-08-27 00:59:41 +0200

    wip

--- src/main/scala/dev/a4i/bsc/etl/common/transform/RasterToVectorTransformationService.scala
+++ src/main/scala/dev/a4i/bsc/etl/common/transform/RasterToVectorTransformationService.scala
@@ -16,7 +16,7 @@ class RasterToVectorTransformationService:
       classificationRanges: Seq[Range[Integer]] = Seq.empty
   ): UIO[SimpleFeatureCollection] =
     ZIO
-      .attemptBlockingInterrupt:
+      .attemptBlocking:
         val effectiveNoDataValues: Seq[Number] =
           if noDataValues.nonEmpty
           then noDataValues
